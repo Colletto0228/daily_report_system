a873d22043ce8435b4e915f1ce298151a10b96f5
小川康太 <harunotk@gmail.com>
2023-02-25 23:04:10 +0900

    トップページアクションの作成

--- src/main/java/models/Report.java
+++ src/main/java/models/Report.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -60,6 +61,7 @@ public class Report {
     @Column(name = JpaConst.REP_COL_TITLE,length = 255,nullable = false)
     private String title;
 
+    @Lob
     @Column(name = JpaConst.REP_COL_CONTENT,nullable = false)
     private String content;
 
